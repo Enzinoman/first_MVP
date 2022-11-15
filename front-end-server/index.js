96bbf40cd6f9336a26fdf4b1ba9f879fbd176eb3
Enzinoman <107152361+Enzinoman@users.noreply.github.com>
2022-11-15 16:34:13 -0700

    13th Commit

--- front-end-server/index.js
+++ front-end-server/index.js
@@ -4,7 +4,8 @@
   const custButton = document.getElementById('custBtn');
   let content = document.getElementById('content');
   const feedbackButton = document.getElementById('feedbackSubmit');
-
+  const apiURL = "https://api-server-wb6x.onrender.com/";
+  //const apiURL = "http://localhost:8000/";
 
   const form = document.getElementById('newCust');
 
@@ -22,7 +23,7 @@
         "customer_feedback": customer_feedback
       };
       console.log(customer);
-      fetch('http://localhost:8000/customers', {
+      fetch(apiURL+'customers', {
         method:"POST",
         headers: {
             'Content-Type': 'application/json'
@@ -59,7 +60,7 @@
         "customer_name": myName
       };
       console.log(custNameEdit);
-      fetch(`http://localhost:8000/customers/${myNameId}`, {
+      fetch(apiURL+`customers/${myNameId}`, {
         method:"PATCH",
         headers: {
             'Content-Type': 'application/json'
@@ -94,7 +95,7 @@
         "customer_name": myPhone
       };
       console.log(custPhoneEdit);
-      fetch(`http://localhost:8000/customers/${myNameId2}`, {
+      fetch(apiURL+`customers/${myNameId2}`, {
         method:"PATCH",
         headers: {
             'Content-Type': 'application/json'
@@ -128,7 +129,7 @@
         "customer_feedback": myFeedback
       };
       console.log(custFeedbackEdit);
-      fetch(`http://localhost:8000/customers/${myNameId3}`, {
+      fetch(apiURL+`customers/${myNameId3}`, {
         method:"PATCH",
         headers: {
             'Content-Type': 'application/json'
@@ -163,7 +164,7 @@
         "id": myNameId4
       };
       console.log(custRecordDelete);
-      fetch(`http://localhost:8000/customers/${myNameId4}`, {
+      fetch(apiURL+`customers/${myNameId4}`, {
         method:"DELETE",
         mode: 'cors',
     })
@@ -195,7 +196,7 @@ function getServicedata(){
     //mode: "no-cors"
   }
   
-  fetch("http://localhost:8000/servicerepair", requestOptions)
+  fetch(apiURL+"servicerepair", requestOptions)
     .then(response => response.json())
     .then(result => {
     
@@ -250,171 +251,6 @@ for(let i = 0; results.length; i++){
 }
 }
 
-
-/* const form = document. getElementById('form');
-
-form.addEventListener('submit', function(e) 
-   e.preventDefault();
-
-   const payLoad = new FormData(form);
-
-   console.log([...payload]);
-
-fetch('http://localhost:8000/servicerepair'){
-
-method: "POST",
-body: payload,
-
-}
-
-.then(res => res,json())
-.then(data => console.log(data))
-.catch(err => console.log(err));
-
-
-}) */
-
-
-
- /* const getVehiclesInShop = fetch("http://localhost:8015/vehicles", requestOptions)
-        .then(response => response.text())
-        .then(result => {
-            
-
-        })
-        .catch(error => console.log('error', error));
-
-
-
-
-
-        
-  const getAwaitingCustomers = fetch("http://localhost:8015/customers", requestOptions)
-        .then(response => response.text())
-        .then(result => {
-            
-
-
-
-
-        })
-        .catch(error => console.log('error', error));
-
-
-
-
-  
-   fetch("https://emojihub.herokuapp.com/api/all/group_face_positive", requestOptions)
-      .then(response => response.json())
-      .then(result => {
-          console.log(result);
-          
-  
-  
-          
-          function convertStringToUnicode(str){
-            let unicode = str;
-            let newString = "";
-            for(let x = 2; x < unicode.length; x++){
-                newString += unicode[x];
-            }
-            return newString;
-          }
-  
-          let form = document.createElement('form');
-          let b = document.createElement('b');
-          let select = document.createElement('select');
-  
-          for(let d = 0; d < result.length; d++){
-            let option = document.createElement('option');
-            option.innerText = result[d].name;
-            unicodeOriginal = result[d].unicode[0];
-            function ChangeEmoji(originalUnicode){
-  
-              let startUnicode = originalUnicode;
-              let intermittentUnicodeString = convertStringToUnicode(startUnicode);
-              let finalUnicode = String.fromCodePoint(parseInt (intermittentUnicodeString, 16));
-              document.getElementById('emojiImgP').innerText = finalUnicode;
-              document.getElementById('nameLabel').innerText= result[d].name;
-  
-            };
-            option.addEventListener('click', ChangeEmoji(unicodeOriginal));
-           
-            
-            
-            select.appendChild(option);
-  
-          
-        }
-        b.appendChild(select);
-        form.appendChild(b);
-        document.getElementById('emojiInput').appendChild(form);
-          
-          
-  
-            
-           /* 
-  <form>  
-  <b> Select you favourite tutorial site using dropdown list </b>  
-  <select id = "myList" onchange = "favTutorial()" >  
-  <option> ---Choose tutorial--- </option>  
-  <option> w3schools </option>  
-  <option> Javatpoint </option>  
-  <option> tutorialspoint </option>  
-  <option> geeksforgeeks </option>  
-  </select>  
-  <p> Your selected tutorial site is:   
-  <input type = "text" id = "favourite" size = "20" </p>  
-  </form>  */
-  
-       
-  
-  
-          /* searchButton.addEventListener("click", function(event){
-              
-              let someMatch = false;
-              let exactmatch = false;
-              
-              let longString = "The following is a list of emojis in our database with similarities to your search. Those emojis whose names are repeated, have multiple word matches to your query, in their names. You can copy and paste one of the emoji names listed and enter it into the input/search: ";
-            for(let i = 0; i < result.length; i++){
-              let string = result[i].name;
-              let strUnicode = result[i].unicode[0];
-              
-              //console.log(typeof result[i].name);
-              if(emojiSearch.value.toLowerCase() == string.toLowerCase() && exactmatch == false){
-                console.log("success!");
-  
-                let finalUnicodeString = convertStringToUnicode(strUnicode);
-                let realUnicode = String.fromCodePoint(parseInt (finalUnicodeString, 16));
-  
-                document.getElementById('emojiImgP').innerText = realUnicode;
-                document.getElementById('nameLabel').innerText= "          " + result[i].name;
-                alert("You've found the emoji you're looking for!");
-                someMatch = true;
-                exactmatch = true;
-                
-              }else {
-  
-                
-                    
-  
-  
-                
-              
-                if(exactmatch == false && i == result.length-1){
-                  alert("no match found. You may use the select / drop-down menu below to pick an emoji or continue to search on your own.")
-      
-                  }
-              
-            }
-            
-            
-          }})
-      })
-       
-      .catch(error => console.log('error', error)
-      
-        ) */
 
 
   
